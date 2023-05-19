b9f90de67e11c34654f5d113f67c357b2de083fa
unknown <loindph36082@fpt.edu.vn>
2023-05-20 02:26:48 +0700

    Bài tập buổi 4

---
+++ BT/app/src/main/java/com/example/bt/constraintlayout2.java
@@ -0,0 +1,13 @@
+package com.example.bt;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class constraintlayout2 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.constraintlayout2);
+    }
+}
---
+++ BT/app/src/main/java/com/example/bt/relativelayout3.java
@@ -0,0 +1,14 @@
+package com.example.bt;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class relativelayout3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.relativelayout3);
+    }
+}
